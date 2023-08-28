87d4b24398e3dafbe1e9e2d6fda7cd4623c8e45d
jey <jeyjeygraham@gmail.com>
2023-08-28 08:55:55 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let pos = {
+    lat: -45,
+    lon: 112
+
+};
+getISS();
+console.log(pos);   
+    
+    async function getISS(){
+        const URL_api = 'https://api.wheretheiss.at/v1/satellites/25544';
+        const response = await fetch (URL_api);
+        const data = await response.json();
+        const {latitude, longitude} = data;
+        
+        console.log(latitude);
+        console.log(longitude);
+    }; 
